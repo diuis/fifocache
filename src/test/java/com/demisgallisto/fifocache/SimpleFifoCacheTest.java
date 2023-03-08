af846ac243cb5bf2c496eea9e3c32bc45697432e
Demis Gallisto <demis.gallisto@wisemotions.com>
2023-03-08 19:53:03 +0100

    cache implementation done

--- src/test/java/com/demisgallisto/fifocache/SimpleFifoCacheTest.java
+++ src/test/java/com/demisgallisto/fifocache/SimpleFifoCacheTest.java
@@ -1,23 +1,44 @@
 package com.demisgallisto.fifocache;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
 class SimpleFifoCacheTest {
 
   private Cache<Integer> sut;
 
+  @BeforeEach
   void setUp() {
-    sut = new SimpleFifoCache();
+    sut = new SimpleFifoCache(3);
   }
 
-  void whenTheCacheIsEmpty() {
+  @Test
+  void whenTheCacheMaxSizeIsLowerThenZeroThenThrowsException() {
+    assertThatIllegalStateException().isThrownBy(() -> new SimpleFifoCache(-1));
+  }
+
+  @Test
+  void whenTheCacheMaxSizeIsEqualsToZeroThenThrowsException() {
+    assertThatIllegalStateException().isThrownBy(() -> new SimpleFifoCache(0));
+  }
+
+  @Test
+  void whenTheCacheIsEmptyThenReturnsAnEmptyList() {
 
     // when
     var result = sut.getAll();
 
     // then
-
+    assertThat(result).isEmpty();
   }
 
-  void whenTheCacheIsntFullThenNoItemsWillBeEvicted() {
+  @Test
+  void whenOneItemIsInsertedIntoTheCacheThenReturnsTheListWithTheSingleItem() {
 
     // given
     sut.put(1);
@@ -26,21 +47,38 @@ class SimpleFifoCacheTest {
     var result = sut.getAll();
 
     // then
-
+    assertThat(result).isEqualTo(List.of(1));
   }
 
-  void whenTheCacheIsFullThenTheOldestItemIsEvicted() {
+  @Test
+  void whenMoreItemsAreInsertedIntoTheCacheAndTheCacheIsntFullThenReturnsAllTheItems() {
+
+    // given
+    sut.put(1);
+    sut.put(2);
+    sut.put(3);
+
+    // when
+    var result = sut.getAll();
+
+    // then
+    assertThat(result).isEqualTo(List.of(3, 2, 1));
+  }
+
+  @Test
+  void whenMoreItemsAreInsertedIntoTheCacheAndTheCacheIsFullThenEvictTheOldestItemsAndReturnsAllTheYoungestItems() {
 
     // given
     sut.put(1);
     sut.put(2);
     sut.put(3);
     sut.put(4);
+    sut.put(2);
 
     // when
     var result = sut.getAll();
 
     // then
-    
+    assertThat(result).isEqualTo(List.of(2, 4, 3));
   }
 }
